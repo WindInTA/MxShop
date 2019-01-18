de21705fe1d3c9ef4af0eb2762b2dd97e6f14b10
davidzhu <hideonc@outlook.com>
2019-01-19 04:29:32 +0800

    增添Media 的地址

--- MxShop/urls.py
+++ MxShop/urls.py
@@ -17,8 +17,12 @@ from django.conf.urls import url
 from django.contrib import admin
 
 import xadmin
+from django.views.static import serve
+from MxShop.settings import  MEDIA_ROOT
+
 
 urlpatterns = [
     # url(r'^admin/', admin.site.urls),
-    url(r'xadmin/', xadmin.site.urls)
+    url(r'xadmin/', xadmin.site.urls),
+    url(r'^media/(<?P<path>.*)$', serve, {"document_root": MEDIA_ROOT}),
 ]
